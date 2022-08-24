065ef2cfed8cd7e51ee1a1ad901ce1b1bc7faa7b
milyuta <mil@mail.ry>
2022-08-24 08:02:21 +0300

    initial commit for 2HW

---
+++ 2HW/Program.cs
@@ -0,0 +1,34 @@
+﻿
+/// 2 homework
+/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+
+*/
+
+/*
+Console.Write("input first integer number: ");
+int number1  = Convert.ToInt32(Console.ReadLine());
+Console.Write("input second integer number: ");
+int number2  = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+    Console.WriteLine($"{number1} is bigger than {number2}");
+}
+else
+{
+    Console.WriteLine($"{number2} is bigger than {number1}");
+}
+*/
+
+
+
