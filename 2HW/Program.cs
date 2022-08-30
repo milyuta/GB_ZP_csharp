e5b50c71772065982800c5c9883ffe21d52fa437
milyuta <mil@mail.ry>
2022-08-30 04:01:17 +0300

    2 hw done

--- 2HW/Program.cs
+++ 2HW/Program.cs
@@ -1,34 +1,87 @@
 ﻿
 /// 2 homework
-/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/* Задача 10: Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.
 456 -> 5
 782 -> 8
 918 -> 1
-Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+*/
+//////////////////
+/*
+int Pack(int num)
+
+{
+    int a;
+    a = num/10%10;
+    return a;
+}
+
+Console.WriteLine("input digit");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = Pack(num);
+
+Console.WriteLine($"{res} is 2-digit number of {num} ");
+
+*/
+
+
+
+/*Задача 13: Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.
 645 -> 5
 78 -> третьей цифры нет
 32679 -> 6
-Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+*/
+/////////////////////////
+/*
+void FindNum(int a)
+{
+    int  hund, b, c;
+    hund = a%1000;
+    b = hund%10; 
+    c = hund/100;
+
+    if(a<=99)
+        Console.WriteLine("a третьей цифры-то нет!");
+    if((a>100)&&(a<1000))
+        Console.WriteLine($"третья цифра {b}");
+    if(a>999)
+        Console.WriteLine($"третья цифра {c}");
+}
+
+Console.WriteLine("input a");
+int x = Convert.ToInt32(Console.ReadLine());
+
+FindNum(x);
+*/
+
+
+/*
+Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
 6 -> да
 7 -> да
 1 -> нет
 
 */
-
+////////////////
 /*
-Console.Write("input first integer number: ");
-int number1  = Convert.ToInt32(Console.ReadLine());
-Console.Write("input second integer number: ");
-int number2  = Convert.ToInt32(Console.ReadLine());
-if(number1 > number2)
+bool Checknum(int x)
+
 {
-    Console.WriteLine($"{number1} is bigger than {number2}");
+    if((x==6)|(x==7))
+    return true;
+    return false;
 }
-else
-{
-    Console.WriteLine($"{number2} is bigger than {number1}");
-}
+
+Console.Write("input the digit");
+int x = Convert.ToInt32(Console.ReadLine());
+bool result = Checknum(x);
+
+if(result)
+    Console.WriteLine($" {x} выходной день недели");
+else 
+    Console.WriteLine($" {x} не выходной день недели");
+
 */
-
-
 
