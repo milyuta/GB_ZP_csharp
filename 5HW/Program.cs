5d03d23eb3cf73cb02d5ae7bd462b0152392c41a
milyuta <110900711+milyuta@users.noreply.github.com>
2022-09-07 18:07:48 +0300

    Update Program.cs

--- 5HW/Program.cs
+++ 5HW/Program.cs
@@ -1,2 +1,159 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+////5hw
+/*
+
+
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+
+/*
+int [] CreateRandomArray(int n, int minVal, int maxVal)
+{
+    int [] arr = new int [n];
+    for(int i=0; i < n; i++)
+    {
+        arr[i] = new Random().Next(minVal, maxVal+1);
+    } 
+    return arr;
+}
+void ShowArray(int [] arr)
+{   
+    Console.Write("[");
+    for (int i = 0; i<arr.Length; i++)
+        Console.Write($"{arr[i]}, ");
+    Console.WriteLine("\b\b ]");
+    Console.WriteLine();
+}
+
+int FindTwoDiv(int[] arra)
+{
+    int j = 0;
+    for(int i = 0; i < arra.Length; i++)
+        if(arra[i]%2 == 0) j ++;
+    return j;
+}
+
+
+ Console.Write("input the sise of your array ");
+ int n = Convert.ToInt32(Console.ReadLine());
+  Console.Write("input minimal value  ");
+ int min = Convert.ToInt32(Console.ReadLine());
+  Console.Write("input maximum value  ");
+ int max= Convert.ToInt32(Console.ReadLine());
+int [] newArray = CreateRandomArray(n, min, max);
+ShowArray(newArray);
+Console.WriteLine($"Ammount of numbers that are divisible by 2 is {FindTwoDiv(newArray)}");
+
+*/
+
+
+
+/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+
+
+/*
+int [] CreateRandomArray(int n, int minVal, int maxVal)
+{
+    int [] arr = new int [n];
+    for(int i=0; i < n; i++)
+    {
+        arr[i] = new Random().Next(minVal, maxVal+1);
+    } 
+    return arr;
+}
+void ShowArray(int [] arr)
+{   
+    Console.Write("[");
+    for (int i = 0; i<arr.Length; i++)
+        Console.Write($"{arr[i]}, ");
+    Console.WriteLine("\b\b ]");
+    Console.WriteLine();
+}
+int FindSum(int[] arra)
+{
+    int i = 0;
+    int sum = 0;
+    for(i = 0; i < arra.Length; i=i+2)
+        sum += arra[i];
+    return sum;
+}
+ Console.Write("input the sise of your array ");
+ int n = Convert.ToInt32(Console.ReadLine());
+  Console.Write("input minimal value  ");
+ int min = Convert.ToInt32(Console.ReadLine());
+  Console.Write("input maximum value  ");
+ int max= Convert.ToInt32(Console.ReadLine());
+int [] newArray = CreateRandomArray(n, min, max);
+ShowArray(newArray);
+Console.WriteLine($"sum of uneven elements is {FindSum(newArray)}");
+*/
+
+/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+
+*/
+
+int [] CreateRandomArray(int n, int minVal, int maxVal)
+{
+    int [] arr = new int [n];
+    for(int i=0; i < n; i++)
+    {
+        arr[i] = new Random().Next(minVal, maxVal+1);
+    } 
+    return arr;
+}
+void ShowArray(int [] arr)
+{   
+    Console.Write("[");
+    for (int i = 0; i<arr.Length; i++)
+        Console.Write($"{arr[i]}, ");
+    Console.WriteLine("\b\b ]");
+    Console.WriteLine();
+}
+
+int FindNegSum(int[] arra)
+{
+    int sum = 0;
+    for(int i = 0; i < arra.Length; i++)
+        if(arra[i] < 0) sum += arra[i];
+    return sum;
+}
+ 
+ Console.Write("input the sise of your array ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ 
+int min =0;
+int max= 100;
+
+int FindMinMaxNums(int [] array) /// если это метод, который не возвращает - void
+
+{
+    int min = 0;
+    int i =0;
+    int result=0;
+for(i = 0; i < array.Length; i++)
+    if(min < array[i]) result = min;
+   
+
+    return result;
+}
+
+//int randNumber = new Random().Next(10, 100);
+//int biggerDigit = FindMinMaxNums(NewArray);
+Console.WriteLine($"Bigger digit of  IS {FindMinMaxNums(array)}");
+
+
+int [] newArray = CreateRandomArray(n, min, max);
+
+ShowArray(newArray);
+Console.WriteLine($"sum of negative elements is {FindNegSum(newArray)}");
